9bec6a86a4d377be5bb68d8d32e20d53f134cd58
Jayanthi-Challa <jayanthichalla123@gmail.com>
2024-08-06 11:57:26 +0530

    postgre update2

--- app.py
+++ app.py
@@ -70,4 +70,6 @@ def view_cart():
     return render_template('view_cart.html', cart=cart)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
